a7161e9266889543683d3d2342cf418c33581cfa
Jorge Gil <jorgemario.gil@gmail.com>
2018-04-13 14:09:32 -0500

    matrizXmatriz completo

--- Cuda/matrizXmatriz/mult.sh
+++ Cuda/matrizXmatriz/mult.sh
@@ -6,11 +6,15 @@
 #SBATCH --nodes=1
 #SBATCH --time=10:00
 #SBATCH --gres=gpu:1
+
+rm mult
  
 export PATH=/usr/local/cuda-8.0/bin${PATH:+:${PATH}}
 export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64/${LD:LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 
 export CUDA_VISIBLE_DEVICES=0
+
+nvcc matrizMatriz.cu -o mult
 
 ./mult matriz1.txt matriz2.txt
 
