7d4b5389a8b6cc8c329224f8e52bf4a12f53401b
aliriox <aliriox@utp.edu.co>
2018-05-09 21:02:37 -0500

    agregando tiempos GPU y CPU, adaptado archivos csv

--- Cuda/Parcial2/cuda_matriz.cu
+++ Cuda/Parcial2/cuda_matriz.cu
@@ -3,6 +3,7 @@
 #include<malloc.h>
 #include<time.h>
 #include<cuda.h>
+#include "archivos_csv.c"
 
 __global__
 void matrizKernell(float* A, float* B, float* C, int rowsA, int colsA, int rowsB, int colsB){
@@ -44,30 +45,62 @@ int main(int argc, char** argv){
         printf("Must be called with the names of the files \n");
         return 1;
     }
+	
+    FILE *fp;
+    fp = fopen(argv[1], "r");
+
+    if(fp==NULL){
+	fputs("File error",stderr);
+	return 1;
+    }
+    fclose(fp);
+	
+    fp = fopen(argv[2], "r");
+
+    if(fp == NULL){
+	fputs("file error", stderr);
+	return 1;
+    }
+    fclose(fp);
+
+    clock_t inicial = clock();
 
     float *A_in, *B_in, *C_out;
     int rowsA, colsA, rowsB, colsB;
 
+    /*
     FILE *f1, *f2;
     f1 = fopen(argv[1], "r");
     f2 = fopen(argv[2], "r");
 
     fscanf(f1, "%d %d", &rowsA, &colsA);
     fscanf(f2, "%d %d", &rowsB, &colsB);
-    
+    */
+
+    rowsA = Detected_rows(fp,argv[1]);
+    colsA = Detected_columns(fp,argv[1]);
+
+    rowsB = Detected_rows(fp,argv[2]);
+    colsB = Detected_columns(fp,argv[2]);
+
+    if(colsA != rowsB){
+        printf("las matrizes no cumplen los requisitos para la multiplicacion entre estas");
+        return 1;
+    }
+
     //CPU
     A_in = (float*)malloc(rowsA * colsA * sizeof(float));
     B_in = (float*)malloc(rowsB * colsB * sizeof(float));
     C_out = (float*)malloc(rowsA * colsB * sizeof(float));
     
-    receive(A_in, f1, rowsA, colsA);    
-    receive(B_in, f2, rowsB, colsB);
+    //receive(A_in, f1, rowsA, colsA);    
+    //receive(B_in, f2, rowsB, colsB);
     //print(A_in, rowsA, colsA);
     
-    if(colsA != rowsB){
-        printf("Debe ser igual el numero de las columnas de A, a las filas de B");
-        return 1;
-    }
+    ExtracData(fp,A_in,argv[1],rowsA,colsA);
+    ExtracData(fp,B_in,argv[2],rowsB,colsB);
+    
+    clock_t GPU_i = clock();
 
     //GPU
     cudaError_t error = cudaSuccess;
@@ -103,6 +136,12 @@ int main(int argc, char** argv){
 
     cudaMemcpy(C_out, d_Cout, rowsA * colsB * sizeof(float), cudaMemcpyDeviceToHost);
     print(C_out, rowsA, colsB);
+
+    clock_t GPU_f = clock();
+
+    clock_t final = clock();
+    printf("tiempo de GPU: %.16g segundos\n", (double)(GPU_f-GPU_i/CLOCKS_PER_SEC));
+    printf("tiempo de CPU: %.16g segundos\n", (double)(final-inicial/CLOCKS_PER_SEC));
     
     free(A_in); free(B_in); free(C_out);
     cudaFree(d_Ain); cudaFree(d_Bin); cudaFree(d_Cout);
