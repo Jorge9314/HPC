7212e4e19025c81f89826363f2ff0026c8faea10
Jorge Gil <jorgemario.gil@gmail.com>
2018-05-16 17:29:43 -0400

    Template y generador de puntos

---
+++ ProyectoFinal/point.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define LLI long long int
+
+int main(){
+    LLI n;
+    cin >> n;
+    cout << n << endl;
+    for(LLI i=0; i<n; i++){
+        cout << rand()%1000 << " " << rand()%1000 << endl;
+    }
+    return 0;
+}
