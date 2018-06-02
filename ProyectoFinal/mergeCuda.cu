9e41cc6b0c6644d9301a476e7ad8b8ba525d99f5
aliriox <aliriox@utp.edu.co>
2018-06-02 11:52:30 -0500

    aborted core dumped

--- ProyectoFinal/mergeCuda.cu
+++ ProyectoFinal/mergeCuda.cu
@@ -197,9 +197,9 @@ void mergesort(long* data, long size, dim3 threadsPerBlock, dim3 blocksPerGrid) 
         }
 
         // Actually call the kernel
-        cout<< "llamando a a GPU"<<endl;
+        std::cout<< "llamando a a GPU"<<std::endl;
         gpu_mergesort<<<blocksPerGrid, threadsPerBlock>>>(A, B, size, width, slices, D_threads, D_blocks);
-        cout<< "saliendo de la GPU"<<endl;
+        std::cout<< "saliendo de la GPU"<<std::endl;
 
         if (verbose)
             std::cout << "call mergesort kernel: " << tm() << " microseconds\n";
