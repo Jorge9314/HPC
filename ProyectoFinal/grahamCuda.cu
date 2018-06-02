29a1d58d265427c6a6b4603a50a40cb28c5caa4b
aliriox <aliriox@utp.edu.co>
2018-06-02 14:22:15 -0500

    start again

--- ProyectoFinal/grahamCuda.cu
+++ ProyectoFinal/grahamCuda.cu
@@ -5,10 +5,6 @@
 #include <iostream>
 #include <stack>
 #include <stdlib.h>
-#include <cuda.h>
-#include <stdio.h>
-#include <malloc.h>
-#include <sys/time.h>
 
 using namespace std;
 
@@ -17,12 +13,9 @@ struct Point
     int x, y;
 };
 
-#define min(a, b) (a < b ? a : b)
-
 // A globle point needed for  sorting points with reference
 // to  the first point Used in compare function of qsort()
 Point p0;
-bool verbose;
 
 // A utility function to find next to top in a stack
 Point nextToTop(stack<Point> &S)
@@ -34,177 +27,8 @@ Point nextToTop(stack<Point> &S)
     return res;
 }
 
-// merge sort
-
-timeval tStart;
-int tm() {
-    timeval tEnd;
-    gettimeofday(&tEnd, 0);
-    int t = (tEnd.tv_sec - tStart.tv_sec) * 1000000 + tEnd.tv_usec - tStart.tv_usec;
-    tStart = tEnd;
-    return t;
-}
-
-//merge sort
-__device__ unsigned int getIdx(dim3* threads, dim3* blocks) {
-    int x;
-    return threadIdx.x +
-           threadIdx.y * (x  = threads->x) +
-           threadIdx.z * (x *= threads->y) +
-           blockIdx.x  * (x *= threads->z) +
-           blockIdx.y  * (x *= blocks->z) +
-           blockIdx.z  * (x *= blocks->y);
-}
-
-//
-// Finally, sort something
-// gets called by gpu_mergesort() for each slice
-//
-__device__ void gpu_bottomUpMerge(long* source, long* dest, long start, long middle, long end) {
-    long i = start;
-    long j = middle;
-    for (long k = start; k < end; k++) {
-        if (i < middle && (j >= end || source[i] < source[j])) {
-            dest[k] = source[i];
-            i++;
-        } else {
-            dest[k] = source[j];
-            j++;
-        }
-    }
-}
-
-//
-// Perform a full mergesort on our section of the data.
-//
-__global__ void gpu_mergesort(long* source, long* dest, long size, long width, long slices, dim3* threads, dim3* blocks) {
-    unsigned int idx = getIdx(threads, blocks);
-    long start = width*idx*slices,
-         middle,
-         end;
-
-    for (long slice = 0; slice < slices; slice++) {
-        if (start >= size)
-            break;
-
-        middle = min(start + (width >> 1), size);
-        end = min(start + width, size);
-        gpu_bottomUpMerge(source, dest, start, middle, end);
-        start += width;
-    }
-}
-
-//mergesortCuda
-void mergesort(long* data, long size, dim3 threadsPerBlock, dim3 blocksPerGrid) {
-
-    //
-    // Allocate two arrays on the GPU
-    // we switch back and forth between them during the sort
-    //
-    long* D_data;
-    long* D_swp;
-    dim3* D_threads;
-    dim3* D_blocks;
-    cudaError_t error = cudaSuccess;
-    // Actually allocate the two arrays
-    tm();
-    std::cout<<"reservando memoria con cudamalloc"<<std::endl;
-    error = cudaMalloc((void**) &D_data, size * sizeof(long));
-    if(error != cudaSuccess){
-           std::cout<<"Error reservando memoria para D_data"<<std::endl;
-     }
-    std::cout<<"pass 1"<<std::endl;
-    error = cudaMalloc((void**) &D_swp, size * sizeof(long));
-    if(error != cudaSuccess){
-           std::cout<<"Error reservando memoria para D_swp"<<std::endl;
-     }
-    std::cout<<"pass 2"<<std::endl;
-    if (verbose)
-        std::cout << "cudaMalloc device lists: " << tm() << " microseconds\n";
-
-    // Copy from our input list into the first array
-    cudaMemcpy(D_data, data, size * sizeof(long), cudaMemcpyHostToDevice);
-    std::cout<<"copy 1"<<std::endl;
-    if (verbose)
-        std::cout << "cudaMemcpy list to device: " << tm() << " microseconds\n";
-
-    //
-    // Copy the thread / block info to the GPU as well
-    //
-    error = cudaMalloc((void**) &D_threads, sizeof(dim3));
-    if(error != cudaSuccess){
-           std::cout<<"Error reservando memoria para D_threads"<<std::endl;
-     }
-    error = cudaMalloc((void**) &D_blocks, sizeof(dim3));
-    if(error != cudaSuccess){
-           std::cout<<"Error reservando memoria para D_blocks"<<std:: endl;
-     }
-
-    std::cout<<"pass t and b"<<std::endl;
-
-    if (verbose)
-        std::cout << "cudaMalloc device thread data: " << tm() << " microseconds\n";
-    cudaMemcpy(D_threads, &threadsPerBlock, sizeof(dim3), cudaMemcpyHostToDevice);
-    cudaMemcpy(D_blocks, &blocksPerGrid, sizeof(dim3), cudaMemcpyHostToDevice);
-
-    std::cout<<"copy t and b"<<std::endl;
-
-    if (verbose)
-        std::cout << "cudaMemcpy thread data to device: " << tm() << " microseconds\n";
-
-    long* A = D_data;
-    long* B = D_swp;
-
-    long nThreads = threadsPerBlock.x * threadsPerBlock.y * threadsPerBlock.z *
-                    blocksPerGrid.x * blocksPerGrid.y * blocksPerGrid.z;
-
-    //
-    // Slice up the list and give pieces of it to each thread, letting the pieces grow
-    // bigger and bigger until the whole list is sorted
-    //
-    std::cout<<"antes del ciclo for extraño"<<std::endl;
-
-    for (int width = 2; width < (size << 1); width <<= 1) {
-        long slices = size / ((nThreads) * width) + 1;
-
-        if (verbose) {
-            std::cout << "mergeSort - width: " << width
-                      << ", slices: " << slices
-                      << ", nThreads: " << nThreads << '\n';
-            tm();
-        }
-
-        // Actually call the kernel
-        std::cout<< "llamando a a GPU"<<std::endl;
-        gpu_mergesort<<<blocksPerGrid, threadsPerBlock>>>(A, B, size, width, slices, D_threads, D_blocks);
-        std::cout<< "saliendo de la GPU"<<std::endl;
-
-        if (verbose)
-            std::cout << "call mergesort kernel: " << tm() << " microseconds\n";
-
-        // Switch the input / output arrays instead of copying them around
-        A = A == D_data ? D_swp : D_data;
-        B = B == D_data ? D_swp : D_data;
-    }
-
-    //
-    // Get the list back from the GPU
-    //
-    tm();
-    cudaMemcpy(data, A, size * sizeof(long), cudaMemcpyDeviceToHost);
-    if (verbose)
-        std::cout << "cudaMemcpy list back to host: " << tm() << " microseconds\n";
-
-
-    // Free the GPU memory
-    cudaFree(A);
-    cudaFree(B);
-    if (verbose)
-        std::cout << "cudaFree: " << tm() << " microseconds\n";
-}
-
 // A utility function to swap two points
-void swap(Point &p1, Point &p2)
+int swap(Point &p1, Point &p2)
 {
     Point temp = p1;
     p1 = p2;
@@ -248,37 +72,6 @@ int compare(const void *vp1, const void *vp2)
    return (o == 2)? -1: 1;
 }
 
-void mainMergeSortCuda(long *v, long n){
-
-    dim3 threadsPerBlock;
-    dim3 blocksPerGrid;
-
-    threadsPerBlock.x = 32;
-    threadsPerBlock.y = 1;
-    threadsPerBlock.z = 1;
-
-    blocksPerGrid.x = 8;
-    blocksPerGrid.y = 1;
-    blocksPerGrid.z = 1;
-
-    //
-    // Read numbers from stdin
-    //
-    long* data;
-    long size = n;
-    data = v;
-
-    // merge-sort the data
-    mergesort(data, size, threadsPerBlock, blocksPerGrid);
-
-    //
-    // Print out the list
-    //
-    for (int i = 0; i < size; i++) {
-        std::cout << data[i] << '\n';
-    }
-}
-
 // Prints convex hull of a set of n points.
 void convexHull(Point points[], int n)
 {
@@ -303,32 +96,7 @@ void convexHull(Point points[], int n)
    // has larger polar angle (in counterclockwise
    // direction) than p1
    p0 = points[0];
-   cout<<points[0].x<<" "<<points[0].y<<endl;
-
-   //qsort(&points[1], n-1, sizeof(Point), compare);
-
-   cout << "sacando distancias"<<endl; 
-   long *distance;
-   distance = (long*)malloc(n-1*sizeof(long));
-
-   //extract distances
-   for(int i = 0; i < n-1; i++){
-    distance[i] = distSq(p0,points[i+1]);
-   }
-
-   long size = n-1;
-   dim3 dimBlock(32,1,1);
-   dim3 dimGrid(8,1,1);
-
-   cout<<"ordenando distancias"<<endl;
-
-   mainMergeSortCuda(distance, size);
-
-   for(int i = 0; i < n-1; i++){
-      cout<<"["<<distance[i]<<"]"<<endl;
-   }
-
-   cout<<"datos ordenados"<<endl;
+   qsort(&points[1], n-1, sizeof(Point), compare);
 
    // If two or more points make same angle with p0,
    // Remove all but the one that is farthest from p0
@@ -380,11 +148,11 @@ void convexHull(Point points[], int n)
        cout << p.x << " " << p.y << endl;
        S.pop();
    }
-   free(distance);
 }
 
 // Driver program to test above functions
 int main(){
+
     int n;
     cin >> n;
     cout << n << endl;
